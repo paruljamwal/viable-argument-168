1e91d0ecc4daddc568ae712c17e605f892884490
paruljamwal <paruljamwal2000@gmail.com>
2022-07-19 18:22:00 +0530

    boiler Palatte

---
+++ src/Components/AllRoutes/MainRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+const MainRoutes = () => {
+  return (
+    <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+    </Routes>
+  )
+}
+
+export default MainRoutes
