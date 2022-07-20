3aa577f952fe08b0a91caec78a00e707c11dd2da
Mohit Patil <97459016+mohit7067@users.noreply.github.com>
2022-07-20 23:14:53 +0530

    demo section done

--- src/Components/AllRoutes/MainRoutes.jsx
+++ src/Components/AllRoutes/MainRoutes.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Demo from "../../Pages/Demo/Demo";
 const MainRoutes = () => {
   return (
     <Routes>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+      <Route></Route>
+      <Route></Route>
+      <Route></Route>
+      <Route></Route>
+      <Route path="/request-a-demo" element={<Demo />}></Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
