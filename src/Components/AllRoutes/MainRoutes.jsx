f546ca05e394c321c8638aacf8d20d6d5ca3b58a
Rohit sahu <96680038+RohitSahuvlog@users.noreply.github.com>
2022-07-21 00:25:59 +0530

    rohitcommitday2

--- src/Components/AllRoutes/MainRoutes.jsx
+++ src/Components/AllRoutes/MainRoutes.jsx
@@ -1,15 +1,16 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Pricingpage from "../../Pages/Pricing/Pricingpage"
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route />
-      <Route />
-      <Route />
-      <Route />
-      <Route />
+        <Route ></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+       <Route path='/price' element={<Pricingpage/>}/>
     </Routes>
-  );
-};
+  )
+}
 
-export default MainRoutes;
+export default MainRoutes
