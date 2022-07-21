9ab47dac6958a02209411b4321623cc6b75661a0
Ahahrukh <rose.sa818@gmail.com>
2022-07-21 22:58:02 +0530

    tested

--- src/Components/AllRoutes/MainRoutes.jsx
+++ src/Components/AllRoutes/MainRoutes.jsx
@@ -4,14 +4,16 @@ import {Routes,Route} from 'react-router-dom'
 import Pricingpage from "../../Pages/Pricing/Pricingpage";
 import Demo from "../../Pages/Demo/Demo";
 import Landing from '../../Pages/Landing/Landing'
+import Customer from '../../Pages/Customer/Customer';
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Landing/>} ></Route>
-        <Route></Route>
+        <Route path='/customer'   element={<Customer/>}></Route>
         <Route></Route>
        <Route path='/price' element={<Pricingpage/>}/>
        <Route path="/request-a-demo" element={<Demo />}></Route>
+      
     </Routes>
   )
 }
