2906adf86c87fa11f2911c2121da5b0d51f20dca
paruljamwal <paruljamwal2000@gmail.com>
2022-07-21 15:42:25 +0530

    Landing Page

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import TopSection from './Sections/TopSection'
-
+import Carousol from './Sections/Carousol'
+import GetMore from './Sections/GetMore'
+import MidSection from './Sections/MidSection'
+import PortFolio from './Sections/PortFolio'
+import Comment from './Sections/Comments'
 const Landing = () => {
   return (
     <div>
       <TopSection/>
-    
+      <MidSection/>
+      <Carousol/>
+      <Comment/>
+      <PortFolio/>
+      <GetMore/>
     </div>
   )
 }
