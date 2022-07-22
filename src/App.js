f7379fde728639c961e7c9a60c0cbf18c06c6eb2
Rajuprajapti <rprajuprajapati16@gmail.com>
2022-07-22 16:15:08 +0530

    first commit day-4

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import "./App.css";
 import MainRoutes from "./Components/AllRoutes/MainRoutes";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
-
+import DetailForm from "./Home/DetailForm";
 
 function App() {
   return (
     <div className="App">
-       <Header /> 
-      <MainRoutes />
-      <Footer />
-
-
+      <DetailForm />
     </div>
   );
 }
